6fb60c46f74ea92b7a1b9928d163bd1638ea7953
tt <tt@Mac.lan>
2026-04-23 04:02:59 -0400

    main

--- js/visuals/change-background.js
+++ js/visuals/change-background.js
@@ -16,7 +16,7 @@ export function initBgSlider() {
     // assign images
     slides.forEach((slide, i) => {
         slide.style.backgroundImage = `url(${images[i]})`;
-        if (images[i] != "imgs/rustic rockymoutains-1024.JPEG"){
+        if(images[i] === "pages/medical-spa-services/media/AS-Botox2000.webp.png"){
             slide.classList.add('xMove')
         }
     });
