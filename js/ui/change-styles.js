92e3819c7389dd133e51d80c6194f44bdc410ed6
tt <tt@Mac.lan>
2026-04-26 08:18:13 -0400

    main

--- js/ui/change-styles.js
+++ js/ui/change-styles.js
@@ -1,18 +1,19 @@
 // change-styles.js
 export function initChangeStyles(){
     // const themes = ["default", "v1","v2"];
-        const themes = ["v2","default"];
+    const themes = ["default","v2","v3"];
+    
     let currentThemeIndex = 0;
-
+    const versionTextEl = document.querySelector('#versionTitle')
     const themeLink = document.getElementById("themeStylesheet");
-
     // Load saved theme
     const savedTheme = localStorage.getItem("theme");
-
+    console.log(savedTheme)
     if (savedTheme && themes.includes(savedTheme)) {
         currentThemeIndex = themes.indexOf(savedTheme);
         themeLink.href = `css/themes/${savedTheme}/main-${savedTheme}.css`;
     }
+    versionTextEl.innerText = savedTheme
 
     // Logo click → switch FULL CSS
     const logo = document.getElementById("madonnaShilouetteLogo");
@@ -24,11 +25,11 @@ export function initChangeStyles(){
             currentThemeIndex = (currentThemeIndex + 1) % themes.length;
 
             const newTheme = themes[currentThemeIndex];
-
             themeLink.href = `css/themes/${newTheme}/main-${newTheme}.css`;
             localStorage.setItem("theme", newTheme);
-
+            
             console.log("Switched to theme:", newTheme);
+            versionTextEl.innerText = newTheme
         });
         logo.addEventListener("keydown", (e) => {
             const key = e.key.toLowerCase()
@@ -41,6 +42,7 @@ export function initChangeStyles(){
                 localStorage.setItem("theme", newTheme);
                 
                 console.log("Switched to theme:", newTheme);
+                versionTextEl.innerText = newTheme
             }
         });
     }
