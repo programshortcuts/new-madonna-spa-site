ceefb72e6023d83926a3597fb936f2649036cf68
tt <tt@Mac.lan>
2026-04-27 17:01:05 -0400

    main

--- js/visuals/change-background.js
+++ js/visuals/change-background.js
@@ -1,13 +1,13 @@
-
+// change-background.js
 export function initBgSlider() {
     const container = document.querySelector('.bg-slider');
     if (!container) return;
 
     const slides = container.querySelectorAll('.bg-slide');
-
+    
     const images = [
         "../../imgs/peptide-header.png",
-        "../../imgs/rustic-rockymoutains-1024.JPEG",
+        // "../../imgs/rustic-rockymoutains-1024.JPEG",
         "../../imgs/rustic-rockymoutains-1024.JPEG"
         // "pages/medical-spa-services/media/AS-Botox2000.webp.png",
         // "pages/ivInfustionTherepy/media/iv-infusion.jpeg"
