eb7a3b140f71803527e60298a271523490104bc6
tt <tt@Mac.lan>
2026-04-27 16:37:39 -0400

    main

--- js/core/inject-content.js
+++ js/core/inject-content.js
@@ -16,8 +16,8 @@ if (!mainLandingPage) {
     throw new Error("Missing .main-landing-page in index.html");
 }
 const DEFAULT_PAGE =
-// "pages/home/home.html";
-"pages/medical-spa-services/medical-spa-services.html";
+"pages/home/home.html";
+// "pages/medical-spa-services/medical-spa-services.html";
 // "pages/products/products.html";
     // "pages/contact/contact.html";
     // "pages/bookings/bookings.html";
