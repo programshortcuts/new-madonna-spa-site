9c0e4e0c5b4607e009b2edbfcb07f1b7602c5f74
tt <tt@Mac.lan>
2026-04-28 11:18:48 -0400

    main

--- js/visuals/change-background.js
+++ js/visuals/change-background.js
@@ -6,9 +6,9 @@ export function initBgSlider() {
     const slides = container.querySelectorAll('.bg-slide');
 
     const images = [
-        "../../imgs/peptide-header.png",
-        "../../imgs/rustic-rockymoutains-1024.JPEG",
-        "../../imgs/rustic-rockymoutains-1024.JPEG"
+        "imgs/rustic-rockymoutains-1024.JPEG",
+        "imgs/peptide-header.png",
+        "imgs/rustic-rockymoutains-1024.JPEG"
         // "pages/medical-spa-services/media/AS-Botox2000.webp.png",
         // "pages/ivInfustionTherepy/media/iv-infusion.jpeg"
         // "../imgs/MadMediSpa-sihlouette.png",
