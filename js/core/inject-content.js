429393a953385ecc564d40061fbbf8136cfe8d3a
tt <tt@Mac.lan>
2026-05-19 23:18:29 -0400

    main

--- js/core/inject-content.js
+++ js/core/inject-content.js
@@ -17,9 +17,9 @@ if (!mainLandingPage) {
     throw new Error("Missing .main-landing-page in index.html");
 }
 const DEFAULT_PAGE =
-// "pages/home/home.html";
+"pages/home/home.html";
 // "pages/medical-spa-services/medical-spa-services.html";
-"pages/products/products.html";
+// "pages/products/products.html";
     // "pages/contact/contact.html";
     // "pages/bookings/bookings.html";
     
