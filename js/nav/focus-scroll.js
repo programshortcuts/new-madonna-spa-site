0d66ba1a06e3871c772b70295b0d4af23f315e4c
tt <tt@Mac.lan>
2026-06-10 09:47:47 -0400

    main

--- js/nav/focus-scroll.js
+++ js/nav/focus-scroll.js
@@ -5,16 +5,6 @@ import { mainLandingPage } from "../core/inject-content.js";
 export function initFocusScroll() {
     const sideNavBtn = document.getElementById('sideNavBtn');
     sideNavBtn?.addEventListener('focus', () => {
-        console.log('focus fired');
-
-        console.log('mainLandingPage:', mainLandingPage);
-        console.log('scrollTop:', mainLandingPage?.scrollTop);
-
-        // mainLandingPage?.scrollTo({
-        //     top: 0,
-        //     behavior: 'smooth'
-        // });
-
         document.querySelectorAll('*').forEach(el => {
             if (el.scrollTop > 0) {
                 console.log(el, el.scrollTop);
