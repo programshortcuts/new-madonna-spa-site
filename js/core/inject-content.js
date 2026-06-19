f419939257636ecff0f7a3ea1b493c9127f7b69f
tt <tt@Mac.lan>
2026-06-19 11:12:39 -0400

    main

--- js/core/inject-content.js
+++ js/core/inject-content.js
@@ -1,6 +1,6 @@
 // 🔥 Ensure correct default page
-export const DEFAULT_PAGE = "pages/home/home.html";
-// export const DEFAULT_PAGE = "pages/medical-spa-services/medical-spa-services.html";
+// export const DEFAULT_PAGE = "pages/home/home.html";
+export const DEFAULT_PAGE = "pages/medical-spa-services/medical-spa-services.html";
 // export const DEFAULT_PAGE = "pages/products/products.html";
 // export const DEFAULT_PAGE = "pages/contact/contact.html";
 // export const DEFAULT_PAGE = "pages/bookings/bookings.html";
